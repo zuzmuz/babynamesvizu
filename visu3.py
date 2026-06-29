845cdf8fc1427c20610922a5f5dade71801323d3
milan <milan.simeon@telecom-paris.fr>
2026-06-29 08:53:12 +0200

    Refinement of visu 3

--- visu3.py
+++ visu3.py
@@ -2,14 +2,14 @@ import pandas as pd
 import altair as alt
 
 # --- Lecture du fichier ---
-names = pd.read_csv("dpt2020.csv", sep=";")
-names.drop(names[names.preusuel == '_PRENOMS_RARES'].index, inplace=True)
+raw = pd.read_csv("dpt2020.csv", sep=";")
+raw = raw[raw["preusuel"] != "_PRENOMS_RARES"]
 
 # --- Somme des effectifs par prénom et par sexe ---
 names = (
-    names.groupby(["preusuel", "sexe"])["nombre"]
-      .sum()
-      .unstack(fill_value=0)
+    raw.groupby(["preusuel", "sexe"])["nombre"]
+    .sum()
+    .unstack(fill_value=0)
 )
 
 # --- Conservation des prénoms voulus seulement,
@@ -20,34 +20,76 @@ names = names[(names["M"] > 200) & (names["F"] > 200)]
 names["ratio"] = names[["M", "F"]].max(axis=1) / names[["M", "F"]].min(axis=1)
 names = names[names["ratio"] <= 50]
 
-# --- Remettre le prénom comme colonne ---
-names = names.reset_index()
+
+# --- Calculs par année
+# raw = raw[raw["annais"] != "XXXX"]
+# raw["annais"] = raw["annais"].astype(int)
+
+unisex_names = set(names.index)
+
+names_by_year = (
+    raw[raw["preusuel"].isin(unisex_names)]
+    .groupby(["preusuel", "annais", "sexe"])["nombre"]
+    .sum()
+    .unstack(fill_value=0)
+    .rename(columns={1: "M", 2: "F"})
+    .reset_index()
+)
+
+# --- Définition du slider
+all_time_toggle = alt.binding_checkbox(name="All time: ")
+all_time_select = alt.param(name="all_time", value=True, bind=all_time_toggle)
+
+year_slider = alt.binding_range(min=1905, max=2020, step=1, name="Central year: ")
+year_select = alt.param(name="yr", value=1960, bind=year_slider)
 
 # --- Création des points ---
-points = alt.Chart(names).mark_circle(size=40).encode(
-    x=alt.X(
-        "M:Q",
-        scale=alt.Scale(type="log"),
-        title="Total of boys"
-    ),
-    y=alt.Y(
-        "F:Q",
-        scale=alt.Scale(type="log"),
-        title="Total of girls"
-    ),
-    tooltip=[
-        alt.Tooltip("preusuel:N", title="Prénom"),
-        alt.Tooltip("M:Q", format=","),
-        alt.Tooltip("F:Q", format=",")
-    ]
-).properties(
-    title="Popularity of unisex names in France (1900–2020)"
-).interactive()
+points = (
+    alt.Chart(names_by_year)
+    .mark_circle(size=40)
+    .encode(
+        x=alt.X("M:Q", scale=alt.Scale(type="log", domainMin=alt.expr("all_time ? 100 : 20"), domainMax=alt.expr("all_time ? 500000 : 200000")), title="Total of boys"),
+        y=alt.Y("F:Q", scale=alt.Scale(type="log", domainMin=alt.expr("all_time ? 100 : 20"), domainMax=alt.expr("all_time ? 500000 : 200000")), title="Total of girls"),
+        color=alt.Color(
+            "ratio:Q",
+            scale=alt.Scale(
+                type="log",
+                scheme="plasma",
+                domain=[1, 50],
+                reverse=True
+            ),
+            legend=alt.Legend(title="M/F ratio")
+        ),
+        tooltip=[
+            alt.Tooltip("preusuel:N", title="Prénom"),
+            alt.Tooltip("M:Q", format=","),
+            alt.Tooltip("F:Q", format=","),
+            alt.Tooltip("ratio:Q", format=".1f", title="Ratio M/F"),
+        ],
+    )
+    .transform_filter(
+    "all_time || ((datum.annais >= yr - 5) & (datum.annais <= yr + 5))"
+    )
+    .transform_aggregate(
+        M="sum(M)",
+        F="sum(F)",
+        groupby=["preusuel"]
+    )
+    .transform_calculate(
+        ratio="max(datum.M, datum.F) / min(datum.M, datum.F)"
+    )
+    .transform_filter(
+    "all_time ? (datum.M > 200 && datum.F > 200) : (datum.M > 20 && datum.F > 20)"
+    )
+    .add_params(year_select, all_time_select)
+    .properties(title="Popularity of unisex names in France (1900–2020)")
+    .interactive()
+)
 
 # --- Création de la diagonale ---
 diag = pd.DataFrame({
-    "x": [100, 1000000],
-    "y": [100, 1000000]
+    "x": [10, 1000000],
+    "y": [10, 1000000]
 })
 ligne = alt.Chart(diag).mark_line().encode(
     x="x:Q",
@@ -56,6 +98,6 @@ ligne = alt.Chart(diag).mark_line().encode(
 
 # --- Composition ---
 chart = (points + ligne)
-chart.save('prenoms_mixtes_france.html', inline=True)
+chart.save('prenoms_mixtes_france.html')
 
 print("Export terminé : prenoms_mixtes_france.html")
